47075137757054481bf4d15a1d5854b083f201a4
milit <milit@DESKTOP-PA5FP0P>
2022-09-15 18:43:25 +0100

    primera version de kata1

---
+++ Kata1/src/main/java/Kata1/Kata1.java
@@ -0,0 +1,11 @@
+package Kata1;
+
+import java.util.Date;
+
+public class Kata1 {
+    
+    public static void main(String[] args) {
+        Person person = new Person("Blas", new Date(49,8,24));
+        System.out.println(person.getName() + " tiene " + person.getAge());
+    }
+}
