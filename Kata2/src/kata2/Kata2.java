1721f831959684d2eca06deb881c93f3d2d23eb4
milit <milit@DESKTOP-PA5FP0P>
2022-10-08 11:24:49 +0100

    Kata2_v1 método 3

--- Kata2/src/kata2/Kata2.java
+++ Kata2/src/kata2/Kata2.java
@@ -1,31 +1,27 @@
 package kata2;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Kata2 {
-
-public static void main(String[] args) {
+    
+    public static void main(String[] args) {
           int [] data = {1, 2, 4, 3, 6, 3, 3, 1, 3, 2, 4, 6, 6, 4};
            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-
+           
+ 
            for (int i = 0; i < data.length; i++) {
                if (map.containsKey(data[i])) {
-                    map.put(data[i], map.get(data[i]) + 1);
-                } else {
-                    map.put(data[i], 1);       
+                map.put(data[i], map.get(data[i]) + 1);
+            } else {
+                map.put(data[i], 1);
         }
     }
-     
-         // Iterar solo sobre claves
-           for (Integer Key : map.keySet()) {
-               System.out.println("Key = " + Key);
-            }
-
-           // Iterar solo sobre valores
-          for (Integer value : map.values()){
-              System.out.println("Value = " + value);     
-          }        
+        Iterator<Map.Entry<Integer, Integer>> entries = map.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry<Integer, Integer> entry = entries.next();
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
     }
 }
