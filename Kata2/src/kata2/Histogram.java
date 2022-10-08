79fcfc3d31f1fa3b40b2b2f4063e5ed06dc470d2
milit <milit@DESKTOP-PA5FP0P>
2022-10-08 12:24:18 +0100

    Kata2_v4 método 1

---
+++ Kata2/src/kata2/Histogram.java
@@ -0,0 +1,31 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Histogram<T> {
+
+    private T [] data;
+    
+    public Histogram(T [] data) {
+        this.data = data;
+    }
+
+    public T[] getData() {
+        return data;
+    }
+    
+    public Map getHistogram(){
+        Map<T, Integer> histogram = new HashMap<T, Integer>();
+        for (T key : data) {
+            if (histogram.containsKey(data[(int)key])) {
+                histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+                
+            } else {
+                histogram.put(data[(int)key], 1);
+            }
+    }
+        return histogram;
+    }
+}
--- Kata2/src/kata2/Kata2.java
+++ Kata2/src/kata2/Kata2.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Kata2 {
     
     public static void main(String[] args) {
-          int [] data = {1, 2, 4, 3, 6, 3, 3, 1, 3, 2, 4, 6, 6, 4};
+          Integer[] data = {1, 2, 4, 3, 6, 3, 3, 1, 3, 2, 4, 6, 6, 4};
            Histogram histo = new Histogram(data);
            Map<Integer,Integer> histogr = histo.getHistogram();
 
