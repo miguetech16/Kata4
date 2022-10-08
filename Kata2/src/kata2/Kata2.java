bea043c3f494c5cd230442e30c6059323b92867a
milit <milit@DESKTOP-PA5FP0P>
2022-10-08 11:24:15 +0100

    Kata2_v1 método 2

--- Kata2/src/kata2/Kata2.java
+++ Kata2/src/kata2/Kata2.java
@@ -5,20 +5,27 @@ import java.util.Map;
 
 public class Kata2 {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
           int [] data = {1, 2, 4, 3, 6, 3, 3, 1, 3, 2, 4, 6, 6, 4};
-           Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+           Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
 
            for (int i = 0; i < data.length; i++) {
-               if (histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i]) + 1);
-            } else {
-                histogram.put(data[i], 1);
+               if (map.containsKey(data[i])) {
+                    map.put(data[i], map.get(data[i]) + 1);
+                } else {
+                    map.put(data[i], 1);       
         }
     }
+     
+         // Iterar solo sobre claves
+           for (Integer Key : map.keySet()) {
+               System.out.println("Key = " + Key);
+            }
 
-          for (Map.Entry<Integer, Integer> entry : histogram.entrySet()){
-              System.out.println(entry.getKey() + " ==> " + entry.getValue());     
+           // Iterar solo sobre valores
+          for (Integer value : map.values()){
+              System.out.println("Value = " + value);     
           }        
     }
 }
